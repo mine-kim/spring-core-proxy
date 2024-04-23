4c588dd0da1f8d8d3f6065c3e36bb18770f62cb5
kmh <minhye0619@gmail.com>
2024-04-24 00:42:49 +0900

    first commit

---
+++ src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -0,0 +1,29 @@
+package hello.proxy.app.v3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController// 스프링은 @controller 또는 @RestController 이 있어야 스프링 컨트롤러로 인식
+@ResponseBody
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+
+    public OrderControllerV3(OrderServiceV3 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v3/no-log")
+    public String noLog() {
+        return null;
+    }
+}
---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,28 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CasheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CasheProxy casheProxy = new CasheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(casheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
