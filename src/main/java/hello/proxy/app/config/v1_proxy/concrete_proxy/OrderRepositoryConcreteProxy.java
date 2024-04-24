3a06ab4802413affb6ba72b22b7ade1a441bf132
kmh <minhye0619@gmail.com>
2024-04-25 00:09:14 +0900

    Proxy pattern & decorator pattern

---
+++ src/main/java/hello/proxy/app/config/v1_proxy/concrete_proxy/OrderRepositoryConcreteProxy.java
@@ -0,0 +1,20 @@
+package hello.proxy.app.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderRepositoryConcreteProxy extends OrderRepositoryV2 {
+
+    private final OrderRepositoryV2 target;
+    private final LogTrace logTrace;
+
+    public OrderRepositoryConcreteProxy(OrderRepositoryV2 target, LogTrace logTrace) {
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public void save(String itemId) {
+        super.save(itemId);
+    }
+}
