7afc58fb45e69b588947c0f5ffc0f4b8c5ff30f7
kmh <minhye0619@gmail.com>
2024-04-28 20:44:56 +0900

    ProxyFactory, PointCut, Advice, Advisor

---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,22 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        log.info("TimeAdvice 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = methodInvocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        log.info("TimeAdvice 종료 resultTime={}", endTime - startTime);
+        return result;
+    }
+}
